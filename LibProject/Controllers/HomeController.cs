9787f4ffc92a00eaef59af2a462eefef5b405183
NGuyen Huu Dung <huudungkd3@gmail.com>
2023-12-21 15:57:40 +0700

    first commit

---
+++ LibProject/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using LibProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LibProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult HomePage()
+        {
+            return View();
+        }
+        [Route("HomePageLogined")]
+        public IActionResult HomePageLogined()
+        {
+            return View();
+        }
+        [Route("SignUp")]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [Route("SignIn")]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+    }
+}
